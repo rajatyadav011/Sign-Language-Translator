09e59d172144a08156312e727608fd247a484db3
Rajat Yadav <129190145+rajatyadav011@users.noreply.github.com>
2025-02-10 12:59:49 +0530

    Update sign_language_translator.ino

--- sign_language_translator.ino
+++ sign_language_translator.ino
@@ -53,24 +53,47 @@ void loop() {
   lcd.setCursor(0, 0);
   lcd.print("Signs: ");
   lcd.setCursor(0, 1);
-
   lcd.print(detectedSigns);
 
   delay(500);
 }
 
 char detectLetter(int flex1, int flex2, int flex3, int flex4, int flex5) {
-  if (flex1 > threshold1 && flex2 < threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 < threshold5) {
-    return 'A';
-  } else if (flex1 < threshold1 && flex2 > threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 < threshold5) {
-    return 'B';
-  } else if (flex1 < threshold1 && flex2 < threshold2 && flex3 > threshold3 && flex4 < threshold4 && flex5 < threshold5) {
-    return 'C';
-  } else if (flex1 < threshold1 && flex2 < threshold2 && flex3 < threshold3 && flex4 > threshold4 && flex5 < threshold5) {
-    return 'D';
-  } else if (flex1 < threshold1 && flex2 < threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 > threshold5) {
-    return 'E';
-  } else {
-    return ' ';
-  }
+  if (flex1 > threshold1 && flex2 < threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 < threshold5) return 'A';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 < threshold5) return 'B';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 > threshold3 && flex4 < threshold4 && flex5 < threshold5) return 'C';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 > threshold3 && flex4 > threshold4 && flex5 < threshold5) return 'D';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 > threshold3 && flex4 > threshold4 && flex5 > threshold5) return 'E';
+  if (flex1 < threshold1 && flex2 < threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 > threshold5) return 'F';
+  if (flex1 < threshold1 && flex2 > threshold2 && flex3 < threshold3 && flex4 > threshold4 && flex5 > threshold5) return 'G';
+  if (flex1 > threshold1 && flex2 < threshold2 && flex3 > threshold3 && flex4 < threshold4 && flex5 > threshold5) return 'H';
+  if (flex1 < threshold1 && flex2 > threshold2 && flex3 > threshold3 && flex4 > threshold4 && flex5 > threshold5) return 'I';
+  if (flex1 > threshold1 && flex2 < threshold2 && flex3 < threshold3 && flex4 > threshold4 && flex5 > threshold5) return 'J';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 < threshold3 && flex4 > threshold4 && flex5 > threshold5) return 'K';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 > threshold3 && flex4 > threshold4 && flex5 > threshold5) return 'L';
+  if (flex1 < threshold1 && flex2 < threshold2 && flex3 > threshold3 && flex4 > threshold4 && flex5 > threshold5) return 'M';
+  if (flex1 < threshold1 && flex2 < threshold2 && flex3 < threshold3 && flex4 > threshold4 && flex5 > threshold5) return 'N';
+  if (flex1 < threshold1 && flex2 < threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 > threshold5) return 'O';
+  if (flex1 > threshold1 && flex2 < threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 > threshold5) return 'P';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 > threshold5) return 'Q';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 > threshold3 && flex4 < threshold4 && flex5 > threshold5) return 'R';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 > threshold3 && flex4 > threshold4 && flex5 > threshold5) return 'S';
+  if (flex1 < threshold1 && flex2 > threshold2 && flex3 > threshold3 && flex4 > threshold4 && flex5 < threshold5) return 'T';
+  if (flex1 < threshold1 && flex2 > threshold2 && flex3 < threshold3 && flex4 > threshold4 && flex5 < threshold5) return 'U';
+  if (flex1 > threshold1 && flex2 < threshold2 && flex3 < threshold3 && flex4 > threshold4 && flex5 < threshold5) return 'V';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 < threshold3 && flex4 > threshold4 && flex5 < threshold5) return 'W';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 > threshold3 && flex4 > threshold4 && flex5 < threshold5) return 'X';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 > threshold3 && flex4 > threshold4 && flex5 > threshold5) return 'Y';
+  if (flex1 < threshold1 && flex2 < threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 < threshold5) return 'Z';
+  if (flex1 < threshold1 && flex2 < threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 < threshold5) return '0';
+  if (flex1 > threshold1 && flex2 < threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 < threshold5) return '1';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 < threshold5) return '2';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 < threshold5) return '3';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 < threshold5) return '4';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 < threshold5) return '5';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 < threshold5) return '6';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 < threshold5) return '7';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 < threshold5) return '8';
+  if (flex1 > threshold1 && flex2 > threshold2 && flex3 < threshold3 && flex4 < threshold4 && flex5 < threshold5) return '9';
+  return ' ';
 }
